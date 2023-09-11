527aa40808355abf28a70ef053650e5bf31630e3
hoangvanlong1999 <hoangvanlong.vn1999@gmail.com>
2023-09-11 21:39:24 +0700

    hoan thanh layout lan 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './includes/Login';
-import UserList from './pages/users/UserList';
 import UserEdit from './pages/users/UserEdit';
 import UserProfile from './pages/users/UserProfile';
 import Borrow from './pages/Borrow';
@@ -15,7 +14,6 @@ function App() {
         <Routes>
           <Route path="/Borrows" element={<Borrow />} />
           <Route path="/Devices" element={<Device />} />
-          <Route path="/" element={<UserList />} />
           <Route path="/users/:id/edit" element={<UserEdit />} />
           <Route path="/users/:id" element={<UserProfile />} />
           <Route path="/Borrows/create" element={<BorrowCreate />} />
